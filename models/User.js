05cacf320a5285bb9ed8655dcafd59a34e404021
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 01:28:02 +0300

    Update User.js

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+
+const UserSchema = mongoose.Schema({
+  username:{
+    type:String,
+    required:true,
+    unique:true,
+    maxlength:100,
+    minlength:3,
+    trim:true
+  },
+  email:{
+    type:String,
+    required:true,
+    unique:true,
+    maxlength:100,
+    minlength:3,
+    trim:true
+  },
+  password:{
+    type:String,
+    required:true,
+    minlength:6
+  },
+  bio:String,
+  imageProfile{
+    type:Object,
+    default:{
+      url:"https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png",
+      publicId:null
+    }
+  },
+  isAdmin:{
+    type:Boolean,
+    default:false
+  },
+  isAccountVerified:{
+    type:Boolean,
+    default:false
+  }
+  
+},{timestamps:true});
+
+const UserModel= mongoose.model("User",UserSchema);
+const validateRegistetUser =(obj) =>{
+  const schema = joi.object({
+    username:joi.string().trim().maxlength(100).minlength(6).unique().required(),
+    email:joi.string().trim().minlength(6).unique().required().email,
+    password;joi.string().trim().minlength(6).required()
+  })
+  return schema.validate(obj);
+}
+module.exports= {
+  UserModel,
+  validateRegisterUser
+}

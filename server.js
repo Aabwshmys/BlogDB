c27de6bbc03822b6722ee1303e99e0724697e435
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 02:05:26 +0300

    Update server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
+const register = require("./router/Register");
+
+app.use(express.json());
+app.use(cors);
+app.use("/auth",register);
+
+
+app.listen(3000,()=>{
+  console.log("server is runing on port 3000");
+});

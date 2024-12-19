f3e4ffeadfada7acba7c0bcd3c7faca16ab2b408
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 01:51:57 +0300

    Update Register.js

--- controller/Register.js
+++ controller/Register.js
@@ -1,3 +1,24 @@
-const async = require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
-const {validateRegisterUser,UserModel} = require("../models/User");
+const { validateRegisterUser, UserModel } = require("../models/User");
+
+module.exports = {
+  createUser: asyncHandler(async (req, res) => {
+    const { error } = validateRegisterUser(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
+    }
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).json({ message: "User Already Exists" });
+    }
+    const hashPassword = bcrypt.hashSync(req.body.password, 10);
+    const create = new UserModel({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword
+    });
+    await create.save();
+    return res.status(200).json({ message: "Create User successfully" });
+  })
+};

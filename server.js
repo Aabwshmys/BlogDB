1e911b056c5471f9a0a162d86d067bb6d0c954f0
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 21:39:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cors());
 
 mongoose.connect("mongodb+srv://alnasr9988:A0994660508a@cluster0.xmfbu.mongodb.net/blogdb?retryWrites=true&w=majority&appName=Cluster0")
-.than(res => console.log("connected db"));
+.then(res => console.log("connected db"));
 
 app.use("/auth",Auth);
 app.use("/users",Users);

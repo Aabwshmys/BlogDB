492dbe274967bbbe6aecf9565286514de916e4c9
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 12:13:49 +0300

    Update Auth.js

--- router/Auth.js
+++ router/Auth.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const controller = require("../controller/Auth");
 
 router.post("/Register",controller.createUser);
+router.post("/Login",controller.login);
 
 module.exports =router;

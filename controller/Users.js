932438417fa21855f7e25219ff074a5268429a2d
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 22:50:16 +0300

    Update Users.js

--- controller/Users.js
+++ controller/Users.js
@@ -1,9 +1,14 @@
-const jwt = require("jsonwebtoken");
+
 const asyncHandler = require("express-async-handler");
 const Users = require("../models/User");
+
 module.exports = {
-  getAllUsers:asyncHandler(async (req,res) => {
-    const users = await Users.find();
-    res.status(200).json(users);
+  getAllUsers: asyncHandler(async (req, res) => {
+    try {
+      const users = await Users.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
   })
-}
+};

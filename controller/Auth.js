cc196d7b66213548c60909f19741eaaecbb300a5
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 22:44:27 +0300

    Update Auth.js

--- controller/Auth.js
+++ controller/Auth.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { validateRegisterUser,validateLoginUser, UserModel } = require("../models/User");
+const { validateRegisterUser, validateLoginUser, UserModel } = require("../models/User");
 
 module.exports = {
   createUser: asyncHandler(async (req, res) => {
@@ -22,21 +22,24 @@ module.exports = {
     await create.save();
     return res.status(200).json({ message: "Create User successfully" });
   }),
-  login:asyncHandler(async (req,res) => {
-    const {error} = validateLoginUser(req.body);
-    if(error){
-      return res.status(400).json({message:error.dateils[0].message});
+
+  login: asyncHandler(async (req, res) => {
+    const { error } = validateLoginUser(req.body);
+    if (error) {
+      return res.status(400).json({ message: error.details[0].message });
     }
-    const cheackUser = await UserModel.findOne({email:req.body.email});
-    if(!cheackUser){
-      return res.status(400).json({message:"Error in Email or Password"});
+
+    const checkUser = await UserModel.findOne({ email: req.body.email });
+    if (!checkUser) {
+      return res.status(400).json({ message: "Error in Email or Password" });
     }
-    const cheackPassword =  bcrypt.compare(req.body.password,cheackUser.password);
-    if(!cheackPassword){
-      return res.status(400).json({message:"Error in Email or Password"});
+
+    const checkPassword = await bcrypt.compare(req.body.password, checkUser.password);
+    if (!checkPassword) {
+      return res.status(400).json({ message: "Error in Email or Password" });
     }
-    const token = cheackUser.generateAuthToken();
-    return res.status(200).json({message:"Logined is successfully",token});
+
+    const token = checkUser.generateAuthToken();
+    return res.status(200).json({ message: "Logined successfully", token });
   })
-  
 };

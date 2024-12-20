9164cc5f33356d788e321eb98d3a18906a95f395
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 22:56:06 +0300

    Update Users.js

--- controller/Users.js
+++ controller/Users.js
@@ -1,11 +1,11 @@
 
 const asyncHandler = require("express-async-handler");
-const Users = require("../models/User");
+const {UserModel} = require("../models/User");
 
 module.exports = {
   getAllUsers: asyncHandler(async (req, res) => {
     try {
-      const users = await Users.find();
+      const users = await UserModel.find();
       res.status(200).json(users);
     } catch (err) {
       res.status(500).json({ message: err.message });

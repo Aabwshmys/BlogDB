63331e749f2d0dd1735b02839fbed1839b20bbc0
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 22:50:54 +0300

    Update Users.js

--- router/Users.js
+++ router/Users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const controller = require("../controller/Users");
 
-router.route("/users").get(controller.getAllUsers);
+router.route("/profile").get(controller.getAllUsers);
 
 module.exports = router;

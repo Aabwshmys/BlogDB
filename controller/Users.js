493769a6fad67c00291d756c5c5545d07c6aa03d
Aabwshmys <aabwshmys84@gmail.com>
2024-12-20 19:48:41 +0300

    Create Users.js

---
+++ controller/Users.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+const asyncHandler = require("express-async-handler");
+const Users = require("../models/User");
+module.exports = {
+  getAllUsers:asyncHandler(async (req,res) => {
+    const users = await Users.find();
+    res.status(200).json(users);
+  });
+}

c425de1ddcd6d239e9376a54db216b3a8b976ee9
Aabwshmys <aabwshmys84@gmail.com>
2024-12-21 18:01:55 +0300

    Update Users.js

--- router/Users.js
+++ router/Users.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const controller = require("../controller/Users");
 const {verifyTokenAndAdmin} = require("../middlewares/verifyToken");
+const validateObjectId = require("../middlewares/validateObjectId");
 
 router.route("/profile").get(verifyTokenAndAdmin,controller.getAllUsers);
+router.route("/profile/:id").get(validateObjectId,controller.getUser);
 
 module.exports = router;

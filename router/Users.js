b46ae0462b2291255bffb91dca98b459324181be
Aabwshmys <aabwshmys84@gmail.com>
2024-12-25 18:54:39 +0300

    Update Users.js

--- router/Users.js
+++ router/Users.js
@@ -2,10 +2,11 @@ const router = require("express").Router();
 const controller = require("../controller/Users");
 const {verifyTokenAndAdmin,verifyTokenAndUserId} = require("../middlewares/verifyToken");
 const validateObjectId = require("../middlewares/ValidateObjectId");
+const photoUpload = require("../middlewares/PhotoUpload");
 
 router.route("/profile").get(verifyTokenAndAdmin,controller.getAllUsers);
 router.route("/profile/:id").get(validateObjectId,controller.getUser);
 router.route("/profile/:id").put(validateObjectId,verifyTokenAndUserId,controller.updateUser);
-router.route("/profile/upload-peofile-photo").post(verifyTokenAndAdmin,controller.uploadProfile);
+router.route("/profile/upload-peofile-photo").post(verifyTokenAndAdmin,photoUpload.single("image"),controller.uploadProfile);
 
 module.exports = router;
